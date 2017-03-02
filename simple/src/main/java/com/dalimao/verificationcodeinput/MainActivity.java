b324dd2c29b9e9770ab7759fa228d3055ef8bec7
guangli.lgl <guangli.lgl@alibaba-inc.com>
2017-03-02 18:29:46 +0800

    feat: 完善simple

--- simple/src/main/java/com/dalimao/verificationcodeinput/MainActivity.java
+++ simple/src/main/java/com/dalimao/verificationcodeinput/MainActivity.java
@@ -2,12 +2,24 @@ package com.dalimao.verificationcodeinput;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.dalimao.corelibrary.VerificationCodeInput;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String  TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        VerificationCodeInput input = (VerificationCodeInput) findViewById(R.id.verificationCodeInput);
+        input.setOnCompleteListener(new VerificationCodeInput.Listener() {
+            @Override
+            public void onComplete(String content) {
+                Log.d(TAG, "完成输入：" + content);
+            }
+        });
     }
 }

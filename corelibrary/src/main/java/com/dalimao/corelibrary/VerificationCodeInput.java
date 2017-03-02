9b2341be6cb7dfc42a623e9e03a904d5d2a0c4b3
guangli.lgl <guangli.lgl@alibaba-inc.com>
2017-03-02 11:02:45 +0800

    fix: 控件库化

---
+++ corelibrary/src/main/java/com/dalimao/corelibrary/VerificationCodeInput.java
@@ -0,0 +1,206 @@
+/*
+ * Copyright (C) 2013 UCWeb Inc. All rights reserved
+ * 本代码版权归UC优视科技所有。
+ * UC游戏交易平台为优视科技（UC）旗下的手机游戏交易平台产品
+ *
+ *
+ */
+
+package com.dalimao.corelibrary;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
+import android.text.Editable;
+import android.text.InputFilter;
+import android.text.InputType;
+import android.text.TextWatcher;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+
+
+
+
+
+public class VerificationCodeInput extends ViewGroup {
+
+    private static final String TAG = "VerificationCodeInput";
+    private int box = 4;
+    private int boxWidth = 120;
+    private int boxHeight = 120;
+    private int childHPadding = 14;
+    private int childVPadding = 14;
+    private Drawable boxBg = null;
+    private EditText currentFocusChild;
+    private Listener listener;
+
+    public VerificationCodeInput(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.vericationCodeInput);
+        box = a.getInt(R.styleable.vericationCodeInput_box, 4);
+
+        childHPadding = (int) a.getDimension(R.styleable.vericationCodeInput_child_h_padding, 0);
+        childVPadding = (int) a.getDimension(R.styleable.vericationCodeInput_child_v_padding, 0);
+        boxBg =  a.getDrawable(R.styleable.vericationCodeInput_box_bg);
+        initViews();
+
+    }
+
+
+    @Override
+    protected void onAttachedToWindow() {
+        super.onAttachedToWindow();
+
+    }
+
+    @Override
+    protected void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+
+
+    }
+
+    private void initViews() {
+        TextWatcher textWatcher = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (s.length() == 1) {
+                    EditText editText = (EditText) getChildAt((currentFocusChild.getId() + 1) % box);
+                    editText.requestFocus();
+                    currentFocusChild = editText;
+
+
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                checkAndCommit();
+
+            }
+
+        };
+
+
+
+        for (int i = 0; i < box; i++) {
+            EditText editText = new EditText(getContext());
+            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(boxWidth, boxHeight);
+            layoutParams.bottomMargin = childVPadding;
+            layoutParams.topMargin = childVPadding;
+            layoutParams.leftMargin = childHPadding;
+            layoutParams.rightMargin = childHPadding;
+            layoutParams.gravity = Gravity.CENTER;
+            editText.setBackground(boxBg);
+            editText.setLayoutParams(layoutParams);
+            editText.setGravity(Gravity.CENTER);
+            editText.setFilters(new InputFilter[]{new InputFilter.LengthFilter(1)});
+            editText.setCursorVisible(false);
+            editText.setInputType(InputType.TYPE_CLASS_NUMBER);
+            editText.setId(i);
+            editText.setEms(1);
+            editText.addTextChangedListener(textWatcher);
+            addView(editText,i);
+            if (i == 0) {
+                editText.requestFocus();
+                currentFocusChild = editText;
+            }
+
+        }
+
+
+    }
+
+    private void checkAndCommit() {
+        StringBuilder stringBuilder = new StringBuilder();
+        boolean full = true;
+        for (int i = 0 ;i < box; i++){
+            EditText editText = (EditText) getChildAt(i);
+            String content = editText.getText().toString();
+            if ( content.length() == 0) {
+                full = false;
+                break;
+            } else {
+                stringBuilder.append(content);
+            }
+
+        }
+        Log.d(TAG, "checkAndCommit:" + stringBuilder.toString());
+        if (full){
+
+            if (listener != null) {
+                listener.onComplete(stringBuilder.toString());
+            }
+
+        }
+    }
+
+    public void setOnCompleteListener(Listener listener){
+        this.listener = listener;
+    }
+
+    @Override
+
+    public LayoutParams generateLayoutParams(AttributeSet attrs) {
+        return new LinearLayout.LayoutParams(getContext(), attrs);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        Log.d(getClass().getName(), "onMeasure");
+        int count = getChildCount();
+
+        for (int i = 0; i < count; i++) {
+            View child = getChildAt(i);
+            this.measureChild(child, widthMeasureSpec, heightMeasureSpec);
+        }
+        if (count > 0) {
+            View child = getChildAt(0);
+            int cHeight = child.getMeasuredHeight();
+            int cWidth = child.getMeasuredWidth();
+            int maxH = cHeight + 2 * childVPadding;
+            int maxW = (cWidth + childHPadding) * box + childHPadding;
+            setMeasuredDimension(resolveSize(maxW, widthMeasureSpec),
+                    resolveSize(maxH, heightMeasureSpec));
+        }
+
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        Log.d(getClass().getName(), "onLayout");
+        int childCount = getChildCount();
+
+        for (int i = 0; i < childCount; i++) {
+            View child = getChildAt(i);
+
+            child.setVisibility(View.VISIBLE);
+            int cWidth = child.getMeasuredWidth();
+            int cHeight = child.getMeasuredHeight();
+            int cl =  (i) * (cWidth + childHPadding);
+            int cr = cl + cWidth;
+            int ct = childVPadding;
+            int cb = ct + cHeight;
+            child.layout(cl, ct, cr, cb);
+        }
+
+
+    }
+
+    public static interface Listener {
+       void onComplete(String content);
+    }
+
+}
+
